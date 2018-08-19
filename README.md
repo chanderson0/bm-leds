50eacf6045e2c73608d4436fb3ac3b14c93e3dba
Chris Anderson <chanderson0@gmail.com>
2018-08-19 17:00:45 -0400

    shoe colors

---
+++ README.md
@@ -0,0 +1,19 @@
+# LED installations for Burning Man 2018
+
+## Dependencies
+
+- Arduino 1.8.5
+- Teensyduino 1.4.2
+
+## Apps
+
+#### Shoe
+
+_TODO: image here_
+
+- Runs on [Pololu A-Star 32U4 Mini SV](https://www.pololu.com/product/3145)
+    - Be sure to follow [board setup instructions](https://www.pololu.com/docs/0J61/6.2)
+    - _TODO: pinout_
+- Dependencies:
+    - [I2C Master Library](http://dsscircuits.com/index.php/articles/66-arduino-i2c-master-library)
+    - [LIS331](https://github.com/szotsaki/LIS331)
--- shoe/shoe.ino
+++ shoe/shoe.ino
@@ -6,7 +6,7 @@
 #define NUM_LEDS 96
 
 #define SCENE_TIME 35
-#define PALETTE_TIME 13
+#define PALETTE_TIME 20
 
 // Note - these are exactly backwards from the wiring
 const uint8_t kLedData[] = {
@@ -481,14 +481,285 @@ DEFINE_GRADIENT_PALETTE(gr66_hult_gp){
     122, 190, 53, 194,
     255, 42, 5, 1};
 
+// Gradient palette "sky_32_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/rafi/tn/sky-32.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( sky_32_gp ) {
+    0, 203,128,151,
+   51, 252,176,212,
+  127, 229,161,228,
+  219, 182,141,240,
+  255, 117,124,223};
+
+// Gradient palette "a_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ds9/tn/a.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( a_gp ) {
+    0,   0,  0,  0,
+   31,   0, 55,  0,
+   63,   0,255, 45,
+  127, 255,  0,255,
+  163, 255,  0, 45,
+  196, 255,  0,  0,
+  255, 255,255,  0};
+
+// Gradient palette "es_seadreams_06_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/sea_dreams/tn/es_seadreams_06.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 20 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_seadreams_06_gp ) {
+    0, 237,207,197,
+   45, 224,187,174,
+  178,  46,199,190,
+  242,   1, 96,172,
+  255,   1, 96,172};
+
+  // Gradient palette "es_autumn_05_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/autumn/tn/es_autumn_05.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 12 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_autumn_05_gp ) {
+    0, 208, 13,  5,
+  204, 255,213,156,
+  255, 255,213,156};
+
+  // Gradient palette "es_rosa_68_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/es/rosa/tn/es_rosa_68.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 16 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( es_rosa_68_gp ) {
+    0, 255,  2,144,
+  127, 255,111,210,
+  188, 255,168,230,
+  255, 255,209,242};
+
+// Gradient palette "knoza_16_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/gacruxa/knoza/tn/knoza-16.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 68 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( knoza_16_gp ) {
+    0,   2,  1,  1,
+    0,   2,  1,  1,
+    2, 237,186,  1,
+   49, 237,186,  1,
+   51, 237,130, 46,
+   98, 237,130, 46,
+  101, 115,  1,  1,
+  116, 115,  1,  1,
+  118,  56, 56,237,
+  136,  56, 56,237,
+  137, 115,  1,  1,
+  153, 115,  1,  1,
+  154, 237,130, 46,
+  203, 237,130, 46,
+  204, 237,186,  1,
+  252, 237,186,  1,
+  255,   2,  1,  1};
+
+// Gradient palette "vilani_20_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/gacruxa/vilani/tn/vilani-20.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 88 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( vilani_20_gp ) {
+    0,  90,105, 93,
+    2, 247,248,247,
+   27,  90,  2,123,
+   28, 247,248,247,
+   54,  90,  2,123,
+   55, 247,248,247,
+   83,  90,  2,123,
+   84, 247,248,247,
+  111,  90,  2,123,
+  112, 247,248,247,
+  140,  90,  2,123,
+  140, 247,248,247,
+  168,  90,  2,123,
+  169, 247,248,247,
+  178, 173, 99,192,
+  193,  90,  2,123,
+  195, 247,248,247,
+  221,  90,  2,123,
+  224, 247,248,247,
+  249,  90,  2,123,
+  252, 247,248,247,
+  255,  45, 58, 48};
+
+  // Gradient palette "higuey_17_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/gacruxa/higuey/tn/higuey-17.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 48 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( higuey_17_gp ) {
+    0, 220, 85, 47,
+   88, 220, 85, 47,
+   89, 194,225,255,
+  100, 194,225,255,
+  101,   1,  1,  1,
+  114,   1,  1,  1,
+  141,   1,  1,  1,
+  152,   1,  1,  1,
+  152, 194,225,255,
+  166, 194,225,255,
+  166,  60, 57,221,
+  255,  60, 57,221};
+
+// Gradient palette "higuey_22_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/gacruxa/higuey/tn/higuey-22.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 48 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( higuey_22_gp ) {
+    0, 167, 75,170,
+   89, 167, 75,170,
+   89, 194,225,255,
+  100, 194,225,255,
+  101,   1,  1,  1,
+  114,   1,  1,  1,
+  141,   1,  1,  1,
+  152,   1,  1,  1,
+  152, 194,225,255,
+  166, 194,225,255,
+  166,  60,178,115,
+  255,  60,178,115};
+
+
+  // Gradient palette "Pastel_Rainbow_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/ggr/tn/Pastel_Rainbow.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 408 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Pastel_Rainbow_gp ) {
+    0, 255,135,123,
+    2, 255,141,123,
+    5, 255,147,123,
+    7, 255,152,123,
+   10, 255,159,123,
+   12, 255,166,123,
+   15, 255,173,123,
+   17, 255,180,123,
+   20, 255,186,123,
+   22, 255,193,123,
+   25, 255,201,123,
+   27, 255,209,123,
+   30, 255,217,123,
+   32, 255,223,123,
+   35, 255,231,123,
+   37, 255,239,123,
+   40, 255,248,123,
+   42, 252,255,123,
+   45, 244,255,123,
+   47, 234,255,123,
+   50, 224,255,123,
+   53, 215,255,123,
+   55, 206,255,123,
+   58, 199,255,123,
+   60, 190,255,123,
+   63, 182,255,123,
+   65, 173,255,123,
+   68, 165,255,123,
+   70, 159,255,123,
+   73, 152,255,123,
+   75, 144,255,123,
+   78, 137,255,123,
+   80, 130,255,123,
+   83, 125,255,123,
+   85, 120,255,125,
+   88, 120,255,132,
+   31, 120,241,255,
+  133, 120,233,255,
+  136, 120,227,255,
+  138, 120,219,255,
+  141, 120,211,255,
+  143, 120,203,255,
+  146, 120,195,255,
+  148, 120,189,255,
+  151, 120,182,255,
+  154, 120,175,255,
+  156, 120,168,255,
+  159, 120,161,255,
+  161, 120,156,255,
+  164, 120,149,255,
+  166, 120,142,255,
+  169, 120,136,255,
+  171, 125,135,255,
+  174, 130,135,255,
+  176, 137,135,255,
+  179, 144,135,255,
+  181, 152,135,255,
+  184, 159,135,255,
+  186, 165,135,255,
+  189, 173,135,255,
+  191, 182,135,255,
+  194, 190,135,255,
+  196, 199,135,255,
+  199, 206,135,255,
+  201, 215,135,255,
+  204, 224,135,255,
+  207, 234,135,255,
+  209, 244,135,255,
+  212, 252,135,255,
+  214, 255,135,247,
+  217, 255,135,237,
+  219, 255,135,228,
+  222, 255,135,219,
+  224, 255,135,212,
+  227, 255,135,203,
+  229, 255,135,194,
+  232, 255,135,186,
+  234, 255,135,178,
+  237, 255,135,172,
+  239, 255,135,164,
+  242, 255,135,156,
+  244, 255,135,149,
+  247, 255,135,142,
+  249, 255,135,137,
+  252, 255,135,130,
+  255, 255,135,123};
+
+  // Gradient palette "Sunset_Real_gp", originally from
+// http://soliton.vm.bytemark.co.uk/pub/cpt-city/nd/atmospheric/tn/Sunset_Real.png.index.html
+// converted for FastLED with gammas (2.6, 2.2, 2.5)
+// Size: 28 bytes of program space.
+
+DEFINE_GRADIENT_PALETTE( Sunset_Real_gp ) {
+    0, 120,  0,  0,
+   22, 179, 22,  0,
+   51, 255,104,  0,
+   85, 167, 22, 18,
+  135, 100,  0,103,
+  198,  16,  0,130,
+  255,   0,  0,160};
+
+
+
 const TProgmemRGBGradientPalettePtr gGradientPalettes[] = {
-    white_gp,
-    gr66_hult_gp,
-    slyvana_gp,
-    goddess_moon_gp,
+    Pastel_Rainbow_gp,
+    es_seadreams_06_gp,
+    higuey_22_gp,
+    higuey_17_gp,
+    es_rosa_68_gp,
+    es_autumn_05_gp,
+    knoza_16_gp,
+    a_gp,
+    Sunset_Real_gp,
     fire_gp,
     My_Heart_Is_Crippled_gp,
     rgi_03_gp,
+    gr66_hult_gp,
+    slyvana_gp,
+    goddess_moon_gp,
+    vilani_20_gp,
+    white_gp,
+    
 };
 
 // Count of how many gradients are defined:
